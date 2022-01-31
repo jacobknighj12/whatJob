68b68aa00d8448915fb7dc4d8573c0cd36914017
Jacob K <jacobknighj@gmail.com>
2022-01-31 14:21:02 +1100

    test merge

--- what-job-react/src/components/NavBar.js
+++ what-job-react/src/components/NavBar.js
@@ -7,6 +7,7 @@ export function NavBar() {
             <button>Login/Signup</button>
             <button>Categories</button>
             <button>Contact us</button>
+            <text></text>
         </div>
     )
 }
