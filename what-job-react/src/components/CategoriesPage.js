bbd264ff734e7c7e1f6728a882bf6d60fe54fd03
Jacob K <jacobknighj@gmail.com>
2022-01-31 19:00:11 +1100

    slate implemented into form, not sure if the slate borders are part of the package may need rethinking

--- what-job-react/src/components/CategoriesPage.js
+++ what-job-react/src/components/CategoriesPage.js
@@ -1,9 +1,31 @@
-import React from 'react'
+
+// Import React dependencies.
+import React, { useState } from 'react'
+// Import the Slate editor factory.
+import { createEditor } from 'slate'
+// Import the Slate components and React plugin.
+import { Slate, Editable, withReact } from 'slate-react'
 
 export function CategoriesPage() {
+    const [editor] = useState(() => withReact(createEditor()))
+    // Add the initial value when setting up our state.
+    const [value, setValue] = useState([
+        {
+            type: 'paragraph',
+            children: [{ text: 'This text is editable click me!' }],
+        },
+    ])
     return (
         <div>
+            <Slate
+                editor={editor}
+                value={value}
+                onChange={newValue => setValue(newValue)}
+            >
+                <Editable />
+            </Slate>
             <form>
+
                 {/* does this need to be in a form? this will need to be majorities filled in by a api call into a 
                 for each category do this probably. as well as below will need a for each category take the the image or we could host it here. not sure */}
                 <label for="Categories">Choose a field:</label>
@@ -12,7 +34,6 @@ export function CategoriesPage() {
                     <option value="youtube gamer">youtube gamer</option>
                 </select>
             </form>
-            <img></img>
             {/* for each category take the image and display it as such in rows */}
         </div>
     )
