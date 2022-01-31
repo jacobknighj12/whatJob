4a2dba186121f2f229a908cd9c821266f438854c
Jacob K <jacobknighj@gmail.com>
2022-01-31 14:16:09 +1100

    begining of skeliton pages

---
+++ what-job-react/src/components/HomePage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export function HomePage() {
+    return (
+        <div>
+            <p>Homepage</p>
+        </div>
+    )
+}
---
+++ what-job-react/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export function NavBar() {
+    return (
+        <div>
+            <button>Home</button>
+            <button>Login/Signup</button>
+            <button>Categories</button>
+            <button>Contact us</button>
+        </div>
+    )
+}
