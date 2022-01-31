0711e9d2603e3e1416040f5094d936da57cf77d4
Jacob K <jacobknighj@gmail.com>
2022-01-31 18:34:39 +1100

    form with basic text explainations for filling them in, no functionality

--- what-job-react/src/App.js
+++ what-job-react/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import { HomePage } from './components/HomePage';
 import { NavBar } from './components/NavBar';
 import { LoginPage } from './components/LoginPage';
 import { SignupPage } from './components/SignupPage';
 import { CategoriesPage } from './components/CategoriesPage';
+import { NewPostPage } from './components/NewPostPage';
 function App() {
   return (
     <div className="App">
@@ -15,6 +15,7 @@ function App() {
       <LoginPage />
       <SignupPage />
       <CategoriesPage />
+      <NewPostPage />
     </div>
   );
 }
