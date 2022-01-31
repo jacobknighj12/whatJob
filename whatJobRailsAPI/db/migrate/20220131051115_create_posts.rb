2a64aeb5fc00563790c46ebf27775e324ebd00c7
katecoderacademy <cam022111@coderacademy.edu.au>
2022-02-01 00:22:42 +1100

    Modified the migration to use decimal instead of integer

--- whatJobRailsAPI/db/migrate/20220131051115_create_posts.rb
+++ whatJobRailsAPI/db/migrate/20220131051115_create_posts.rb
@@ -1,18 +1,19 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :intro
+      t.string :title, null: false
+      t.string :intro, null: false
       t.string :day_to_day
       t.string :career_path
-      t.int :expected_salary_range_from
-      t.int :expected_salary_range_to
-      t.int :difficulty
+      t.decimal :expected_salary_range_from
+      t.decimal :expected_salary_range_to
+      t.decimal :difficulty
       t.string :job_security
       t.string :industry_growth
-      t.int :upvotes
-      t.int :downvotes
-      t.string :role_destription
+      t.string :work_life_balance
+      t.decimal :upvotes
+      t.decimal :downvotes
+      t.string :role_details
       t.string :job_description
 
       t.timestamps
