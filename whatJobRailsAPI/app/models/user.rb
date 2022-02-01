adb3e2f124fe451de1985628e7a1bfc185d2e112
katecoderacademy <cam022111@coderacademy.edu.au>
2022-02-01 23:27:36 +1100

    Generated migration to add firstname and lastname to users

--- whatJobRailsAPI/app/models/user.rb
+++ whatJobRailsAPI/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
          :jwt_authenticatable,
          :registerable,
          jwt_revocation_strategy: JwtDenylist
+  has_secure_password
+
+validates :username, presence: true, uniqueness: true
 end
--- whatJobRailsAPI/db/seeds.rb
+++ whatJobRailsAPI/db/seeds.rb
@@ -6,7 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-categories = ["Accounting", "Administration & Office Support", "Advertising, Arts & Media", "Banking & Financial Services", "Call Centre & Customer Service", "CEO & General Management", "Community Services & Development", "Construction", "Consulting & Strategy", "Design & Architecture", "Education & Training", "Engineering", "Farming, Animals & Conservation", "Government & Defence", "Healthcare & Medical",  "Hospitality & Tourism", "Human Resources & Recruitment", "Information & Communication Technology", "Insurance & Superannuation", "Legal", "Manufacturing, Transport & Logistics", "Marketing Communications", "Mining, Resources & Energy", "Real Estate & Property", "Retail & Consumer Products Sales", "Science & Technology", "Self Employment", "Sport & Recreation", "Trades & Services"]
+if User.count == 0
+
+    if User.count == 0 
+        User.create(username: "Alex", email:"alex@test.com", password: "password1", password_confirmation: "password1")
+        User.create(username: "Alex2", email:"alex2@test.com", password: "password2", password_confirmation: "password2")
+    end
+
+
 
 if Category.all.length == 0
     categories.each do |category|
@@ -14,3 +21,9 @@ if Category.all.length == 0
         puts "created #{category} category"
     end
 end
+
+categories = ["Accounting", "Administration & Office Support", "Advertising, Arts & Media", "Banking & Financial Services", "Call Centre & Customer Service", "CEO & General Management", "Community Services & Development", "Construction", "Consulting & Strategy", "Design & Architecture", "Education & Training", "Engineering", "Farming, Animals & Conservation", "Government & Defence", "Healthcare & Medical",  "Hospitality & Tourism", "Human Resources & Recruitment", "Information & Communication Technology", "Insurance & Superannuation", "Legal", "Manufacturing, Transport & Logistics", "Marketing Communications", "Mining, Resources & Energy", "Real Estate & Property", "Retail & Consumer Products Sales", "Science & Technology", "Self Employment", "Sport & Recreation", "Trades & Services"]
+
+
+
+
