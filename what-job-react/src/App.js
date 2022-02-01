058bbaf20d9c475c628f0dd54ec0b8325896f360
Jacob Knight <78334099+jacobknighj12@users.noreply.github.com>
2022-02-02 09:39:29 +1100

    AccountPage created, attempted to add in useContext but commented out for now (#10)

--- what-job-react/src/App.js
+++ what-job-react/src/App.js
@@ -1,13 +1,17 @@
 
-import './App.css';
+import React, { useContext, useState } from 'react'
+import "./App.css";
+import { ContactUs } from "./components/ContactUs";
 import { HomePage } from './components/HomePage';
 import { NavBar } from './components/NavBar';
 import { LoginPage } from './components/LoginPage';
 import { SignupPage } from './components/SignupPage';
 import { CategoriesPage } from './components/CategoriesPage';
 import { NewPostPage } from './components/NewPostPage';
+import { AccountPage } from "./components/AccountPage";
 
 function App() {
+  // const [darkMode, setDarkMode] = useContext(false)
   return (
     <div className="App">
       <header className="App-header">
@@ -16,12 +20,9 @@ function App() {
       <HomePage />
       <ContactUs />
       <CategoriesPage />
-      <LoginPage />
-      <SignupPage />
-      <CategoriesPage />
       <NewPostPage />
       <ContactUs />
-
+      <AccountPage />
     </div>
   );
 }
