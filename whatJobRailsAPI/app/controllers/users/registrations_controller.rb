b4480fcb95b5dc014145faf007d5f372ceb92bc8
katecoderacademy <cam022111@coderacademy.edu.au>
2022-02-05 00:53:49 +1100

    Putting user auth under API, creating some rspec_rails tests, modifying models to work with Postman

--- whatJobRailsAPI/app/controllers/users/registrations_controller.rb
+++ whatJobRailsAPI/app/controllers/users/registrations_controller.rb
@@ -2,10 +2,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :first_name, :last_name)
+    params.permit(:email, :password, :first_name, :last_name)
   end
-
-
 
   private
 
