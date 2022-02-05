f42cee902b5626305bca37010d4691a1ed5976f9
katecoderacademy <cam022111@coderacademy.edu.au>
2022-02-05 23:31:32 +1100

    Adding regex validation to models'

--- whatJobRailsAPI/app/models/contactmessage.rb
+++ whatJobRailsAPI/app/models/contactmessage.rb
@@ -1,2 +1,8 @@
 class Contactmessage < ApplicationRecord
+
+
+    validates_format_of :name with: /[a-z\s.-]/i
+    validates_format_of :message, with: /\A[a-zA-Z0-9 ,.;'"+-?!%$]*(?:\R[a-zA-Z0-9 ,.;'"+-?!%$]*)*\z/
+    validates_format_of :intro, with: /\A[a-zA-Z0-9 ,.;'"+-?!%$]*(?:\R[a-zA-Z0-9 ,.;'"+-?!%$]*)*\z/
+    validates :email,  presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
--- whatJobRailsAPI/app/models/user.rb
+++ whatJobRailsAPI/app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
          has_many :category
          has_many :posts
 
+         validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true
+         validates_format_of :first_name, :last_name, with: /[a-z\s.-]/i
+        #  validates_format_of :last_name, with: /[a-z\s.-]/i
 
-         validates :email, presence: true, uniqueness: true
 end
