a3e76f3a85adbd37d0a7c9b0326578db89e6d3bd
lgunds6 <lgunds6@gmail.com>
2022-02-05 15:28:02 +1100

    removed my personal email from files, updated views forms, and removed preview data

--- whatJobRailsAPI/spec/mailers/previews/contactmessage_mailer_preview.rb
+++ whatJobRailsAPI/spec/mailers/previews/contactmessage_mailer_preview.rb
@@ -1,8 +1,4 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contactmessage_mailer
 class ContactmessageMailerPreview < ActionMailer::Preview
-    def new_message_email
-        @contactmessage = Contactmessage.new(name: 'Luke', email: 'luke@email.com', message: 'Testing to see if it works')
-        ContactmessageMailer.with(message: @contactmessage).new_message_email
-    end
-
+   
 end
