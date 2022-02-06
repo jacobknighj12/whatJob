559d7e30331271c2d7130fb7b87e1487e4d73b61
Jacob K <jacobknighj@gmail.com>
2022-02-06 19:55:59 +1100

    changed a few elements names

--- what-job-react/src/components/DeepDivePage.js
+++ what-job-react/src/components/DeepDivePage.js
@@ -5,11 +5,11 @@ export function DeepDivePage() {
     const [likes, setLikes] = useState(0);
     const [disLikes, setDisLikes] = useState(0);
     const [posts, setPosts] = useState([]);
-    const [category, setCategory] = useState(2);
+    const [post_id, setPost_Id] = useState(2);
     useEffect(() => {
         var config = {
             method: 'get',
-            url: `http://localhost:3000/api/posts/${category}`,
+            url: `http://localhost:3000/api/posts/${post_id}`,
             headers: {}
         };
 
@@ -36,11 +36,11 @@ export function DeepDivePage() {
             {posts.map((post) => (
                 <div key={post.id}>
                     {(() => {
-                        console.log('post.category_id')
-                        console.log(post.category_id)
-                        console.log('category')
-                        console.log(category)
-                        if (post.category_id == category) {
+                        console.log('post.post_id')
+                        console.log(post.id)
+                        console.log('post_id')
+                        console.log(post_id)
+                        if (post.id === post_id) {
                             return (
                                 <div><div>
                                     <h2>Deep dive on: {post.title}</h2>
