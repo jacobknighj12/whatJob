9373630f8598ff918c16371bf57f7f7e7a2a477b
lgunds6 <lgunds6@gmail.com>
2022-02-08 17:13:31 +1100

    Rspec tests for createcontactmessage and reportalert passed

--- whatJobRailsAPI/spec/models/contactmessage_spec.rb
+++ whatJobRailsAPI/spec/models/contactmessage_spec.rb
@@ -2,12 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Contactmessage, type: :model do
   it 'successfully creates a new contactmessage' do
-    testContactmessage = Post.new
+    testContactmessage = Contactmessage.new
     testContactmessage.name = 'test name'
     testContactmessage.email = 'test email'
     testContactmessage.message = 'test message'
     expect(testContactmessage.name).to eq('test name') 
     expect(testContactmessage.email).to eq('test email') 
-    expect(testContactmessage.message).to eq(test message) 
+    expect(testContactmessage.message).to eq('test message') 
   end
 end
--- whatJobRailsAPI/spec/models/reportalert_spec.rb
+++ whatJobRailsAPI/spec/models/reportalert_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Reportalert, type: :model do
   it 'successfully creates a new report alert' do
     testReportalert = Reportalert.new
-    testReportalert.id = 'test id'
-    expect(testReportalert.id ).to eq('test id') 
+    testReportalert.id = (1)
+    expect(testReportalert.id ).to eq(1) 
 
   end
 end
