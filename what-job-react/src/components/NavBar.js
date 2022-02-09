35c66fd6960acb880217b6b183e1adba526598cf
Jacob Knight <78334099+jacobknighj12@users.noreply.github.com>
2022-02-09 15:09:12 +1100

    Feature/back too intergration (#38)
    * hard reset my local major issue
    
    * signup and signin now display correct returns
    
    * signup and signin now display correct returns
    
    * persist the return data from login
    
    * Delete credentials.yml.enc
    
    * Delete master.key:Zone.Identifier
    
    * Delete credentials.yml.enc:Zone.Identifier
    
    * attemp 1 to pull from local storage
    
    * technically working however no user is given or can be put in unsure of what is required so it does not create currently
    
    * found the source of the issue the headers have not been sending auth for unknown reason or auth cannot be found
    
    * broke down walls today, login now is fully functional well. mostly, it saves token to localstorage and is now used for newposts to auth a new post being created and it gets stored and displayed fully
    
    * added missing feilds
    
    * small edits gave up on conditional rendering of login signup

--- what-job-react/src/components/NavBar.js
+++ what-job-react/src/components/NavBar.js
@@ -5,18 +5,15 @@ export function NavBar() {
             <img src='whatJob-logo.JPG' style={{ width: 600 }}></img>
             <img src=''></img>
             <a href='/'>
-                <button>Home</button>
-            </a>
-            {/* if user isLoggedIn == true */}
-            <a href='/Signup'>
-                <button>Login/Signup</button></a>
-            {/* else  */}
-            <a href='/Account'>
-                <button>My Account</button></a>
+                <button>Home</button></a>
             <a href='/Categories'>
                 <button>Categories</button></a>
             <a href='/ContactUs'>
                 <button>Contact us</button></a>
-        </div>
+            <a href='/Account'>
+                <button>My Account</button></a>
+            < a href='/Login' ><button>Login</button></a>
+            <a href='/Signup'><button>Signup</button></a>
+        </div >
     )
 }
--- what-job-react/src/components/NewPostPage.js
+++ what-job-react/src/components/NewPostPage.js
@@ -6,31 +6,39 @@ export function NewPostPage() {
     const [intro, setIntro] = useState("Short introduction for yourself, how long you have worked in this position and an example of a great day and a bad day at your job.");
     const [day_to_day, setDay_to_day] = useState("Go over the boring/exciting stuff that you often overlook but most of your days consist of");
     const [career_path, setCareer_path] = useState("How did you get to this position, study, luck, hardship, Nepotism. As well as ways that you know that enable a person to enter this field today.");
+    const [industry_growth, setIndustry_growth] = useState("How fast is this industry growing, or is it shrinking? do they have good or bad entry into the industry due to new things happening?.");
+    const [work_life_balance, setWork_life_balance] = useState("how much time do you have to yourself after work? do you do extra hours constantly? do you have lots of overtime? can you get time off easily? how many hours from getting up till you get home is your average day? including travel.");
 
-    function handleSubmit() {
+    function handleSubmit(event) {
+        event.preventDefault();
         var FormData = require('form-data');
         var data = new FormData();
-        data.append('category_id', '3');
-        data.append('title', 'Another new Title');
-        data.append('intro', 'Another new Intro');
-        data.append('day_to_day', 'This is day to day');
-        data.append('career_path', 'This is career path');
-        data.append('expected_salary_range_from', '169690');
-        data.append('expected_salary_range_to', '2069');
-
+        data.append('category_id', `${event.target.category_id.value}`);
+        data.append('title', `${event.target.title.value}`);
+        data.append('intro', `${intro}`);
+        data.append('day_to_day', `${day_to_day}`);
+        data.append('career_path', `${career_path}`);
+        data.append('industry_growth', `${industry_growth}`);
+        data.append('work_life_balance', `${work_life_balance}`);
+        data.append('expected_salary_range_from', `${event.target.expected_salary_range_from.value}`);
+        data.append('expected_salary_range_to', `${event.target.expected_salary_range_to.value}`);
+        data.append('difficulty', `${event.target.difficulty.value}`);
+        // console.log(event.target.difficulty.value)
+        // console.log(event.target.expected_salary_range_from.value)
         var config = {
             method: 'post',
-            url: 'localhost:3000/api/posts',
+            url: 'http://localhost:3000/api/posts',
             headers: {
-                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwic2NwIjoidXNlciIsImF1ZCI6bnVsbCwiaWF0IjoxNjQzODY2NTQ5LCJleHAiOjE2NDM4NzAxNDksImp0aSI6IjVlYjJiMDliLTBjZmMtNGE1NS1iNGY3LWVmOWY4Njc2MDYyOSJ9.lzJpRo9D6xBUMeK2zCtfLT-3LDDXAO6324_DpvrSaM4',
-                ...data.getHeaders()
+                'Authorization': localStorage.getItem('user')
+                // ...data.getHeaders
             },
             data: data
         };
 
         axios(config)
             .then(function (response) {
-                console.log(JSON.stringify(response.data));
+                console.log(response.data);
+
             })
             .catch(function (error) {
                 console.log(error);
@@ -39,10 +47,16 @@ export function NewPostPage() {
     return (
         <div>
             <h2>New post</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
+                <label>Category</label>
+                <select name="category_id">
+                    <option value="1">gamer</option>
+                    <option value="2">flamer</option>
+                    <option value="3">blamer</option>
+                </select>
 
                 <label>Role title</label>
-                <input></input>
+                <input type='text' name='title'></input>
 
                 <div className="container"><label>Intro</label>
                     <MDEditor
@@ -57,7 +71,7 @@ export function NewPostPage() {
 
                 <label>Day to day</label>
 
-                <div className="container"><label>Intro</label>
+                <div className="container">
                     <MDEditor
                         value={day_to_day}
                         onChange={setDay_to_day}
@@ -70,7 +84,7 @@ export function NewPostPage() {
 
                 <label>Career Path</label>
 
-                <div className="container"><label>Intro</label>
+                <div className="container">
                     <MDEditor
                         value={career_path}
                         onChange={setCareer_path}
@@ -80,11 +94,41 @@ export function NewPostPage() {
                     />
 
                 </div>
-                <label>expected salary</label>
-                <input></input>
-                <label>difficulty of the position</label>
-                <select >
-                    <option></option>
+
+                <label>Industry growth</label>
+
+                <div className="container">
+                    <MDEditor
+                        value={industry_growth}
+                        onChange={setIndustry_growth}
+                        previewOptions={{
+                            rehypePlugins: [[rehypeSanitize]],
+                        }}
+                    />
+
+                </div>
+
+                <label>Work Life Balance</label>
+
+                <div className="container">
+                    <MDEditor
+                        value={work_life_balance}
+                        onChange={setWork_life_balance}
+                        previewOptions={{
+                            rehypePlugins: [[rehypeSanitize]],
+                        }}
+                    />
+
+                </div>
+
+                <label>expected salary from $</label>
+                <input type='number' name='expected_salary_range_from'></input>
+                <label>to</label>
+                <input type='number' name='expected_salary_range_to'></input>
+                <br />
+                <label>difficulty of the position</label><br />
+                <select type='number' name='difficulty'>
+
                     <option value={1}>1/10</option>
                     <option value={2}>2/10</option>
                     <option value={3} >3/10</option>
@@ -95,8 +139,8 @@ export function NewPostPage() {
                     <option value={8} >8/10</option>
                     <option value={9} >9/10</option>
                     <option value={10} >10/10</option>
-                </select>
-                <button onSubmit={handleSubmit}>publish</button>
+                </select><br />
+                <button >publish</button>
             </form>
         </div>
     )
