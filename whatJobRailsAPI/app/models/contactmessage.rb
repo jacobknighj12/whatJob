3c7fa4230187ed6e6f5cba06e911e2aad2e5114a
katecoderacademy <cam022111@coderacademy.edu.au>
2022-02-10 00:21:48 +1100

    Modified Regex to include brackets

--- whatJobRailsAPI/app/models/contactmessage.rb
+++ whatJobRailsAPI/app/models/contactmessage.rb
@@ -3,7 +3,7 @@ class Contactmessage < ApplicationRecord
     # Name not required but can only be alpha characters
     validates_format_of :name, with: /[a-z\s.-]/i
     # Message is required, but most characters are acceptable.
-    validates_format_of :message, allow_blank: false, with: /\A[a-zA-Z0-9 ,.;'"+-?!%$]*(?:\R[a-zA-Z0-9 ,.;'"+-?!%$]*)*\z/
+    validates_format_of :message, allow_blank: false, with: /[A-Za-z0-9 _\-.,;#&!?"'\\/$()%]*/
     # Email is required, formatting using the inbuilt email Regex
     validates :email,  allow_blank: false,  format: { with: URI::MailTo::EMAIL_REGEXP }
 end
