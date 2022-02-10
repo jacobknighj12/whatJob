7f73ff24f890884988fe06f30b819d10ac831de5
lgunds6 <lgunds6@gmail.com>
2022-02-10 15:29:01 +1100

    Commiting all files from main branch

--- what-job-react/src/components/PostPage.js
+++ what-job-react/src/components/PostPage.js
@@ -5,34 +5,11 @@ export function JobPosts(props) {
   const [likes, setLikes] = useState(0);
   const [disLikes, setDisLikes] = useState(0);
   const [posts, setPosts] = useState([]);
-<<<<<<< HEAD
-
-  const handleReportClick = (event) => {
-    event.preventDefault();
-      var data = values;
-  
-      var config = {
-        method: "post",
-        url: "http://localhost:3000/api/contactmessage",
-        headers: {},
-        data: data,
-      };
-  
-      axios(config)
-        .then(function (response) {
-          console.log(JSON.stringify(response.data));
-        })
-        .catch(function (error) {
-          console.log(error);
-        });
-    };
-  };
-
-=======
   const [category, setCategory] = useState(1);
->>>>>>> 20f41646243a5d4116113012114bcae77f108129
   useEffect(() => {
-    axios.get("http://localhost:3000/api/posts").then((res) => setPosts(res.data));
+    axios
+      .get("http://localhost:3000/api/posts")
+      .then((res) => setPosts(res.data));
     console.log(posts);
   }, []);
 
@@ -43,35 +20,14 @@ export function JobPosts(props) {
     setDisLikes((prevDisLikes) => prevDisLikes + 1);
   };
   function handleCategory(event) {
-    setCategory(event.target.value)
+    setCategory(event.target.value);
   }
 
-<<<<<<< HEAD
-  function displayAllPosts() {
-    var config = {
-      method: "get",
-      url: "http://localhost:3000/api/posts",
-      headers: {},
-    };
-
-    axios(config)
-      .then(function (response) {
-        setText(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
-  displayAllPosts();
-=======
-
-
->>>>>>> 20f41646243a5d4116113012114bcae77f108129
   return (
     <>
       <div>
         <form onChange={handleCategory}>
-          <label >Choose a field:</label>
+          <label>Choose a field:</label>
           <select id="Jobs" name="Jobs" size="1">
             <option value={1}>gamer</option>
             <option value={2}>youtube gamer</option>
@@ -86,60 +42,43 @@ export function JobPosts(props) {
             {posts.map((post) => (
               <div key={post.id}>
                 {(() => {
-                  console.log('post.category_id')
-                  console.log(post.category_id)
-                  console.log('category')
-                  console.log(category)
+                  console.log("post.category_id");
+                  console.log(post.category_id);
+                  console.log("category");
+                  console.log(category);
                   if (post.category_id == category) {
                     return (
                       <div>
+                        <div>User: {post.user_id}</div>
+                        <div>Title: {post.title}</div>
+                        <div>Introduction: {post.intro}</div>
+                        <div>Day to day: {post.day_to_day}</div>
+                        <div>Job difficulty: {post.difficulty}/10</div>
                         <div>
-                          User: {post.user_id}
-                        </div><div>
-                          Title: {post.title}
-                        </div><div>
-                          Introduction: {post.intro}
-                        </div><div>
-                          Day to day: {post.day_to_day}
-                        </div><div>
-                          Job difficulty: {post.difficulty}/10
-                        </div><div>
-                          Expected salary range from: {post.expected_salary_range_from} to: {post.expected_salary_range_to}
-                        </div><div>
-                          Industry growth: {post.industry_growth}
-                        </div><div>
-                          Work life balance: {post.work_life_balance}
-                        </div><div>
-                        </div><div>
-                          Career Path: {post.career_path}
-                        </div><div>
-                          good post: {post.upvotes}
-                        </div><div>
-                          bad post: {post.downvotes}
+                          Expected salary range from:{" "}
+                          {post.expected_salary_range_from} to:{" "}
+                          {post.expected_salary_range_to}
                         </div>
-                        <button><a>Link to Deep Dive</a></button>
+                        <div>Industry growth: {post.industry_growth}</div>
+                        <div>Work life balance: {post.work_life_balance}</div>
+                        <div></div>
+                        <div>Career Path: {post.career_path}</div>
+                        <div>good post: {post.upvotes}</div>
+                        <div>bad post: {post.downvotes}</div>
+                        <button>
+                          <a>Link to Deep Dive</a>
+                        </button>
                         <div> _</div>
-                      </div>)
+                      </div>
+                    );
                   }
                 })()}
               </div>
-
-            ))
-            }
+            ))}
           </div>
         </div>
-
-<<<<<<< HEAD
-        <div> Post from database inserted here</div>
-        <a>Link to Deep Dive for post</a>
-      </div>
-      <p className="text">{text}</p>
-=======
-
-
       </div>
 
->>>>>>> 20f41646243a5d4116113012114bcae77f108129
       <div>
         <button onClick={handleIncrementLikes}>(Bootstrap thumbs up)</button>
         <h5>Count is {likes}</h5>
@@ -149,9 +88,6 @@ export function JobPosts(props) {
       <button onClick={() => setLikes(0)}></button>
       <div></div>
       <button onClick={() => setDisLikes(0)}></button>
-      <div>
-        <button>Report</button>
-      </div>
     </>
   );
 }
